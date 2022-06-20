2460b9af103cf61df219d6ae9bb3d2f5edc35e30
abshaibu <shaibuabdulsalam56@gmail.com>
2022-06-20 19:38:27 +0100

    changed age to height

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let myName = 'Shaibu Abdulsalam';
-let age = 24;
+let height = 200;
 let country = 'Nigeria';
 
-alert(`My name is ${myName} and I am ${age} years old. I am from ${country}`);
+alert(`My name is ${myName} and I am ${height}cm tall. I am from ${country}`);
